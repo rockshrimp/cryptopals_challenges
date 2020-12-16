fc4e69136d1c2078a00700b51ed983a2f9ae71f4
Lucas <lucasplissonnier@gmail.com>
2020-12-16 16:25:06 +0100

    Changed functions to use byte instead of str

--- set_1/single_byte_xor.py
+++ set_1/single_byte_xor.py
@@ -1,27 +1,29 @@
 from collections import Counter
 
 
-def single_byte_xor(value, hex_string):
-    s = bytes.fromhex(hex_string)
-    return ''.join([chr(value ^ byte) for byte in s])
+def single_byte_xor(value, s):
+    return bytes([value ^ byte for byte in s])
+
+
+# English character frequencies
+eng_letter_frequencies = {
+    'a': .08167, 'b': .01492, 'c': .02782, 'd': .04253,
+    'e': .12702, 'f': .02228, 'g': .02015, 'h': .06094,
+    'i': .06094, 'j': .00153, 'k': .00772, 'l': .04025,
+    'm': .02406, 'n': .06749, 'o': .07507, 'p': .01929,
+    'q': .00095, 'r': .05987, 's': .06327, 't': .09056,
+    'u': .02758, 'v': .00978, 'w': .02360, 'x': .00150,
+    'y': .01974, 'z': .00074, ' ': .18288
+}
+eng_letter_bytes_frequencies = {ord(k): v for k, v in eng_letter_frequencies.items()}
 
 
 def get_english_score(s):
-    # English character frequencies
-    eng_letter_frequencies = {
-        'a': .08167, 'b': .01492, 'c': .02782, 'd': .04253,
-        'e': .12702, 'f': .02228, 'g': .02015, 'h': .06094,
-        'i': .06094, 'j': .00153, 'k': .00772, 'l': .04025,
-        'm': .02406, 'n': .06749, 'o': .07507, 'p': .01929,
-        'q': .00095, 'r': .05987, 's': .06327, 't': .09056,
-        'u': .02758, 'v': .00978, 'w': .02360, 'x': .00150,
-        'y': .01974, 'z': .00074, ' ': .18288
-    }
     s_frequencies = Counter(s)
 
     score = 0
     for letter in s_frequencies:
-        score += abs(s_frequencies[letter] / len(s) - eng_letter_frequencies.get(letter, 0))
+        score += abs(s_frequencies[letter] / len(s) - eng_letter_bytes_frequencies.get(letter, 0))
     return score
 
 
@@ -38,5 +40,5 @@ def guess_single_char_xor(s):
 
 
 if __name__ == '__main__':
-    cipher = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    cipher = bytes.fromhex('1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736')
     print(guess_single_char_xor(cipher))
